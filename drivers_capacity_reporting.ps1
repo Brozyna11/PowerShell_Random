15b43398a0db1fe992d30064b51446927ec683f5
Brozyna11 <aleksander.brozyna@gmail.com>
2020-01-28 20:07:41 +0100

    first commit

---
+++ drivers_capacity_reporting.ps1
@@ -0,0 +1,6 @@
+﻿# setting the script to run as administrator
+Set-ExecutionPolicy Unrestricted
+# calling Get-Date cmdlet as saving it into log.txt file
+Get-Date | Out-file F:\PowerShell\log.txt -Append
+# calling Get-PSDrive in order to get capacity of the drives together with exact time indicated by the Get-Date function and saving it into log.txt file
+Get-PSDrive -PSProvider FileSystem | Out-File F:\PowerShell\log.txt -Append
---
+++ files_sorting.ps1
@@ -0,0 +1,21 @@
+﻿# get the files to be moved and saving them into variable "files" as list
+$files = Get-ChildItem 'E:\Zdjecia\Pictures\Z telefonu' -Recurse | where {!$_.PsIsContainer}
+$targetPath = 'E:\Zdjecia\Pictures\Albumy'
+
+foreach ($file in $files)
+{
+# creating variables month and year
+$year = $file.LastWriteTime.Year.ToString()
+$month = $file.LastWriteTime.Month.ToString()
+ 
+# creating variable to store new directory
+$Directory = $targetPath + "\" + $year + "\" + $month
+# Create directory if it doesn't exsist
+if (!(Test-Path $Directory))
+{
+New-Item $directory -type directory
+}
+ 
+# Move File to new location
+$file | Move-Item -Destination $Directory
+}
